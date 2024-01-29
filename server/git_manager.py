64ccd11b172e1cd0b858c6da59d0e40bce35c505
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-01-29 15:00:10 +0200

    Create repo function

--- server/git_manager.py
+++ server/git_manager.py
@@ -1,4 +1,8 @@
+import typing
 import git
+
+
+conf_file_path = "conf/gitolite.conf"
 
 
 class GitManager:
@@ -6,25 +10,38 @@ class GitManager:
         self.repo = git.Repo(path)
         self.path = path
 
-    def add_ssh_key(self, user_name: str, ssh_key: str):
-        file_name = f"keydir/{user_name}.pub"
-        with open(f"{self.path}/{file_name}", "w+") as ssh_key_file:
-            ssh_key_file.write(ssh_key)
-
-        add_file = [file_name]
-        self.repo.index.add(add_file)
+    def _commit_files(self, commit_message: str, files_to_commit: typing.List[str]):
+        self.repo.index.add(files_to_commit)
         self.repo.git.commit(
             "-m",
-            f"Add {user_name} ssh key",
+            commit_message,
         )
 
         origin = self.repo.remote(name="origin")
         origin.push()
 
-    def create_repo(self,repo_name:str, user: str, publc: bool):
+    def add_ssh_key(self, user_name: str, ssh_key: str):
+        file_name = f"keydir/{user_name}.pub"
+        with open(f"{self.path}/{file_name}", "w+") as ssh_key_file:
+            ssh_key_file.write(ssh_key)
+        self._commit_files(f"Add {user_name} ssh key", [file_name])
 
+    def create_repo(self, repo_name: str, user: str, public: bool):
+        with open(f"{self.path}/{conf_file_path}", "a") as conf_file:
+            conf_file.write(
+                f"""
+repo {repo_name}
+    RW+     =   {user}
+"""
+            )
+            if public:
+                conf_file.write("""    RW+     =   @all""")
+        self._commit_files(
+            f"Add new {'public' if public else 'private'} repo {repo_name} for user {user}",
+            [conf_file_path],
+        )
 
 
 if __name__ == "__main__":
     gm = GitManager("../gitolite-admin")
-    gm.add_ssh_key("Reef", "fddshkdjshgklh")
+    gm.create_repo("TestRepo", "itamarsch", True)
